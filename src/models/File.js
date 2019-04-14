73227cc4c503e2b8de632fb71872e3610a53b3b3
thobiasvicente <thobiascontact@gmail.com>
2019-04-14 17:30:20 -0300

    adding environment variables

--- src/models/File.js
+++ src/models/File.js
@@ -17,8 +17,10 @@ const File = new mongoose.Schema({
         toJSON: { virtuals: true }
     });
 
-File.virtual('url').get(function(){
-    return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
+File.virtual('url').get(function () {
+    const url = process.env.URL || "http://localhost:3333"
+
+    return `${url}/files/${encodeURIComponent(this.path)}`;
 })
 
 module.exports = mongoose.model("File", File);
