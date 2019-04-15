3806ad0697e4892936a51d3d8ac8cec328168c3f
thobiasvicente <thobiascontact@gmail.com>
2019-04-14 22:23:33 -0300

    aaaa

--- src/models/File.js
+++ src/models/File.js
@@ -1,34 +1,26 @@
 const mongoose = require("mongoose");
 
-
 const File = new mongoose.Schema({
-
     title: {
-
         type: String,
-        required: true,
-
+        required: true
     },
     path: {
         type: String,
-        required: true,
-    },
-
-}, {
+        required: true
+    }
+},
+    {
         timestamps: true,
         toObject: { virtuals: true },
-        toJSON: { virtuals: true },
-    });
+        toJSON: { virtuals: true }
+    }
+);
 
+File.virtual("url").get(function () {
+    const url = process.env.URL || "http://localhost:3333";
 
+    return `${url}/files/${encodeURIComponent(this.path)}`;
+});
 
-File.virtual('url').get(function () {
-    const url = process.env.URL || 'http://localhost/3333'
-
-    return `${url}http://localhost/3333/files/${encodeURIComponent(this.path)}`
-
-})
-
-
-
-module.exports = mongoose.model('File', File);
+module.exports = mongoose.model("File", File);
